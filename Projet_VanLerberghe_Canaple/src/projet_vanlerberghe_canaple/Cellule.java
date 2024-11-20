d42508d99e8ac321d59c401c5ca4eb40a8d0070f
louison_vl <louison_vl@localhost>
2024-11-20 15:19:13 +0100

    création classe cellule

---
+++ Projet_VanLerberghe_Canaple/src/projet_vanlerberghe_canaple/Cellule.java
@@ -0,0 +1,29 @@
+/*
+ * Mini projet 
+ * Louison van Lerberghe/Adrien Canaple 
+ * TDB
+ * novembre 2024
+ */
+package projet_vanlerberghe_canaple;
+
+/**
+ *
+ * @author louison_vl
+ */
+public class Cellule {
+    
+    private boolean presenceBombe;
+    private boolean devoilee;
+    private int NbBombesAdjacentes;
+
+    
+    public boolean getPresenceBombe() {
+        return presenceBombe;
+    }
+
+    public int getNbBombesAdjacentes() {
+        return NbBombesAdjacentes;
+    }
+    
+    
+}
