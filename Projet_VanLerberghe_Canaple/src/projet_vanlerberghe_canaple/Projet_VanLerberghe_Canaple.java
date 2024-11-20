ec05ff5cdf228ff1c72796225706c9d7fa12040b
adrie <adrie@10.3.62.170>
2024-11-20 14:47:41 +0100

    création mini projet

---
+++ Projet_VanLerberghe_Canaple/src/projet_vanlerberghe_canaple/Projet_VanLerberghe_Canaple.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet_vanlerberghe_canaple;
+
+/**
+ *
+ * @author adrie
+ */
+public class Projet_VanLerberghe_Canaple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
